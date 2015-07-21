5a83a84a71e1b791ec7999cbb8f1aee51cd1ec97
michaelregister <michael@innovocative.com>
2015-07-21 15:32:26 -0500

    added typings

---
+++ XDemographics/typings/leaflet.tileLayer.ts
@@ -0,0 +1,31 @@
+﻿declare module L {
+    export interface TileLayerStatic extends ClassStatic {
+        betterWms(url: string, options?: any): L.TileLayer.WMS;
+    }
+    export var TileLayer: TileLayerStatic;
+    export interface TileLayer extends ILayer, IEventPowered<TileLayer> {
+        betterWms(url: string, options?: any): L.TileLayer.WMS;
+    }
+    module TileLayer {
+        export interface WMS extends TileLayer {
+
+            setParams(params: WMS, noRedraw?: boolean): WMS;
+        }
+    }
+
+    export interface TileLayerFactory {
+
+        betterWms(url: string, options?: any): L.TileLayer.WMS;
+    }
+    export var tileLayer: TileLayerFactory;
+}
+declare module L {
+
+    export interface ILayer {
+        userAdded: boolean;
+        _container: any;
+    }
+}
+
+
+
